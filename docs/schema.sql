4ad924373d59e04da5e95804bed8f033cba1c3e0
Ben Youngblood <bx.youngblood@gmail.com>
2011-06-30 10:12:15 -0700

    fix sql errors

--- docs/schema.sql
+++ docs/schema.sql
@@ -6,9 +6,9 @@ CREATE TABLE IF NOT EXISTS `issue` (
   `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `status` enum('open','closed') NOT NULL DEFAULT 'open',
-  `project` int(11) NOT NULL,
-  `created_by` int(11) NOT NULL,
-  `assigned_to` int(11) DEFAULT NULL,
+  `project` int(11) UNSIGNED NOT NULL,
+  `created_by` int(11) UNSIGNED NOT NULL,
+  `assigned_to` int(11) UNSIGNED DEFAULT NULL,
   `created_time` datetime NOT NULL,
   `last_update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`issue_id`)
@@ -18,14 +18,14 @@ CREATE TABLE IF NOT EXISTS `project` (
   `project_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`project_id`),
-  UNIQUE KEY `project_name` (`project_name`)
+  UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` int(11) NOT NULL DEFAULT '1',
+  `role` int(11) UNSIGNED NOT NULL DEFAULT '1',
   `last_login` datetime DEFAULT NULL,
   `last_ip` int(11) DEFAULT NULL,
   `register_time` datetime NOT NULL,
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `user_role` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 CREATE TABLE `issues`.`label` (
-  `label_id` UNSIGNED INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `label_id` INT( 11 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   `text` VARCHAR( 50 ) NOT NULL ,
   `color` VARCHAR( 50 ) NOT NULL
 ) ENGINE = InnoDB;
@@ -62,7 +62,7 @@ ALTER TABLE `issue_label_linker`
 ADD FOREIGN KEY (`label_id`) REFERENCES `issues`.`label` (`label_id`)
 ON DELETE CASCADE ON UPDATE CASCADE;
 
-INSERT INTO `user_role` (`role_id`, `role_name`) VALUES
+INSERT INTO `user_role` (`role_id`, `name`) VALUES
 (1, 'user');
 
 
