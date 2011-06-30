5c653bd85bbd7403143bcdeb909535337d7fc1cf
Ben Youngblood <bx.youngblood@gmail.com>
2011-06-30 10:44:57 -0700

    role and label forms

---
+++ application/modules/default/forms/Role/Base.php
@@ -0,0 +1,30 @@
+<?php
+class Default_Form_Role_Base extends Issues_FormAbstract
+{
+    public function init()
+    {
+        $this->addElement('text', 'name', array(
+            'filters'       => array('StringTrim'),
+            'validators'    => array(
+                array('StringLength', true, array(3, 255)),
+                array('Db_NoRecordExists', true, array(
+                    'adapter'   => Zend_Registry::get('Default_DiContainer')
+                        ->getRoleMapper()
+                        ->getReadAdapter(),
+                    'table'     => Zend_Registry::get('Default_DiContainer')
+                        ->getRoleMapper()
+                        ->getTableName(),
+                    'field'     => 'name'
+                ))
+            ),
+            'required'      => true,
+            'label'         => 'Role Name'
+        ));
+
+        $this->addElement('submit', 'submit', array(
+            'required' => false,
+            'ignore'   => true,
+            'decorators' => array('ViewHelper',array('HtmlTag', array('tag' => 'dd', 'id' => 'form-submit')))
+        ));
+    }
+}
