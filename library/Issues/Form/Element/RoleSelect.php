f109372a1e6c29a19347fe5d98d83440ca458aac
Ben Youngblood <bx.youngblood@gmail.com>
2011-07-08 09:44:45 -0700

    select the current user's roles by default

--- library/Issues/Form/Element/RoleSelect.php
+++ library/Issues/Form/Element/RoleSelect.php
@@ -6,5 +6,10 @@ class Issues_Form_Element_RoleSelect extends Zend_Form_Element_Multiselect
         $roles = Zend_Registry::get('Default_DiContainer')->getRoleService()
             ->getRolesForForm();
         $this->setMultiOptions($roles);
+
+        $currentRoles = Zend_Registry::get('Default_DiContainer')->getUserService()
+            ->getIdentity()->getRoles();
+        $this->setValue($currentRoles);
+
     }
 }
