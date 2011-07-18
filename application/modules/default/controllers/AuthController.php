4eecbaa41e213a16fb9f8d4ec0d6834659442533
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2011-07-18 12:32:40 -0700

    Started work on merging login and register pages into one

--- application/modules/default/controllers/AuthController.php
+++ application/modules/default/controllers/AuthController.php
@@ -6,15 +6,14 @@ class Default_AuthController extends Zend_Controller_Action
         $this->_userService = Zend_Registry::get('Default_DiContainer')->getUserService();
         $fm = $this->getHelper('FlashMessenger')->setNamespace('loginForm')->getMessages(); 
         $this->view->loginForm = (count($fm) > 0) ? $fm[0] : $this->getLoginForm();
+        $this->view->registerForm = (count($fm) > 0) ? $fm[0] : $this->getRegisterForm();
 
         $this->_aclService = Zend_Registry::get('Default_DiContainer')->getAclService();
     }
-
-    public function loginAction()
+    
+    public function indexAction()
     {
-        if (!$this->_aclService->isAllowed('user', 'login')) {
-            return $this->_helper->redirector('index', 'index');
-        }
+        // Index...
     }
 
     public function logoutAction()
@@ -22,24 +21,58 @@ class Default_AuthController extends Zend_Controller_Action
         $this->_userService->logout();
         return $this->_helper->redirector('index','index');
     }
-
-    public function authenticateAction()
+    
+    public function getLoginForm()
     {
+        return $this->_userService->getLoginForm()->setAction($this->_helper->url->direct('authenticate-post'));
+    }
+
+    public function getRegisterForm()
+    {
+        return $this->_userService->getRegisterForm()->setAction($this->_helper->url->direct('register-post'));
+    }
+
+    public function authenticatePostAction()
+    {
+        // ACL Check for authentication action
+        if (!$this->_aclService->isAllowed('auth', 'login')) {
+            return $this->_helper->redirector('index', 'index');
+        }
+        
         $form = $this->view->loginForm;
         $request = $this->getRequest();
-        if (!$request->isPost()) return $this->_helper->redirector('login');
+        if (!$request->isPost()) return $this->_helper->redirector('index');
         $form->populate($request->getPost());
         $vals = $form->getValues();
         if (false === $this->_userService->authenticate($vals['username'], $vals['password'])) {
             $form->setDescription($this->view->translate('login_failed'));
             $this->_helper->FlashMessenger->setNamespace('loginForm')->addMessage($form);
-            $this->_helper->redirector('login');
+            $this->_helper->redirector('index');
         }
         return $this->_helper->redirector('index', 'index');
     }
-
-    public function getLoginForm()
+    
+    public function registerPostAction()
     {
-        return $this->_userService->getLoginForm()->setAction($this->_helper->url->direct('authenticate'));
+        // ACL Check for registration action
+        if (!$this->_aclService->isAllowed('user', 'register')) {
+            return $this->_helper->redirector('index', 'index');
+        }
+        
+        $form = $this->view->registerForm;
+        $request = $this->getRequest();
+        if (!$request->isPost()) return $this->_helper->redirector('index');
+        if (false === $form->isValid($request->getPost())) {
+            $form->setDescription($this->view->translate('registration_failed'));
+            $this->_helper->FlashMessenger->setNamespace('registerForm')->addMessage($form);
+            $this->_helper->redirector('index');
+        }
+        if (!$this->_userService->createFromForm($form)) {
+            $form->setDescription($this->view->translate('registration_failed'));
+            $this->_helper->FlashMessenger->setNamespace('registerForm')->addMessage($form);
+            return $this->_helper->redirector('index');
+        }
+        return $this->_helper->redirector('index', 'auth');
     }
+
 }
